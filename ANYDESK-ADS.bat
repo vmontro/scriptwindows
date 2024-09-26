55381f9d307c71816bdd8916b608df90471b1e17
Vincenzo Montrone <49622862+vmontro@users.noreply.github.com>
2024-09-26 16:08:48 +0200

    Add files via upload

---
+++ ANYDESK-ADS.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal
+taskkill /F /IM anydesk.exe
+set "folderRoaming=C:\Users\%username%\AppData\Roaming\AnyDesk"
+
+:: Controlla se la cartella esiste
+if exist "%folderRoaming%" (
+    echo Cancellazione della cartella: %folderRoaming%
+    rmdir /s /q "%folderRoaming%"
+    echo Cartella Anydesk Roaming cancellata con successo.
+) else (
+    echo Anydesk Roaming non esistente.
+)
+
+set "folderProgramData=C:\ProgramData\AnyDesk"
+
+:: Controlla se la cartella esiste
+if exist "%folderProgramData%" (
+    echo Cancellazione della cartella: %folderProgramData%
+    rmdir /s /q "%folderProgramData%"
+    echo Cartella Anydesk ProgramData cancellata con successo.
+) else (
+    echo Anydesk ProgramData non esistente.
+)
+
+endlocal
